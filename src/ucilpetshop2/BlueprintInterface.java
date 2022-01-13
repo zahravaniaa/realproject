148d134879d407d2ebb37e091a41b8f8f6d0e18f
zahravaniaa <zahraivaniaa@gmail.com>
2022-01-13 10:05:34 +0700

    first commit

---
+++ src/ucilpetshop2/BlueprintInterface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ucilpetshop2;
+
+/**
+ *
+ * @author prati
+ */
+interface BlueprintInter {
+    void opening(String katakata);
+    
+}
